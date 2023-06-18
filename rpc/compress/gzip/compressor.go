517e86fe423deadaf63ecb909f4713e64ff38690
tini <1315827371@qq.com>
2023-06-18 10:30:23 +0800

    compress

---
+++ rpc/compress/gzip/compressor.go
@@ -0,0 +1,18 @@
+package gzip
+
+type Compressor struct{}
+
+func (c *Compressor) Code() uint8 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Compressor) Compress(data []byte) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Compressor) UnCompress(data []byte) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
